fb3f913b7cc9a96806f8f57e8078c36411a71992
mkolyn <mkolyn@gmail.com>
2019-04-18 16:41:39 +0300

    choose city

---
+++ OnlineCinema/Controllers/CoreController.cs
@@ -0,0 +1,27 @@
+﻿using OnlineCinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineCinema.Controllers
+{
+    public class CoreController : Controller
+    {
+        private CityContext cityDb = new CityContext();
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            SetViewBagData();
+        }
+
+        public void SetViewBagData()
+        {
+            ViewBag.Cities = cityDb.Cities.ToList();
+            ViewBag.City = Core.GetCityId() > 0 ? cityDb.Cities.Find(Core.GetCityId()).Name : "";
+        }
+    }
+}
