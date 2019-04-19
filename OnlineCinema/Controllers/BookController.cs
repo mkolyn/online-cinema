0f4b1f846110aee2279c035174c6cc047b8e1615
mkolyn <mkolyn@gmail.com>
2019-04-19 14:13:54 +0300

    book place

---
+++ OnlineCinema/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using OnlineCinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineCinema.Controllers
+{
+    public class BookController : CoreController
+    {
+        private UserContext userDb = new UserContext();
+        private CinemaHallMovieContext cinemaHallMovieDb = new CinemaHallMovieContext();
+        private CinemaHallPlaceContext cinemaHallPlaceDb = new CinemaHallPlaceContext();
+
+        public ActionResult Index(int id)
+        {
+            CinemaHallMovie cinemaHallMovie = cinemaHallMovieDb.CinemaHallMovies.Find(id);
+            if (cinemaHallMovie == null)
+            {
+                return HttpNotFound();
+            }
+
+            CinemaHall cinemaHall = cinemaHallMovieDb.CinemaHalls.Find(cinemaHallMovie.CinemaHallID);
+            Cinema cinema = cinemaHallMovieDb.Cinemas.Find(cinemaHall.CinemaID);
+            Movie movie = cinemaHallMovieDb.Movies.Find(cinemaHallMovie.MovieID);
+
+            DateTime date = cinemaHallMovie.Date;
+
+            ViewBag.cinemaName = cinema.Name;
+            ViewBag.cinemaHallName = cinemaHall.Name;
+            ViewBag.movieName = movie.Name;
+            ViewBag.date = new DateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute, 0);
+
+            CinemaHallPlaceData cinemaHallPlaceData = cinemaHallPlaceDb.GetCinemaHallPlacesData(cinemaHall.ID);
+
+            ViewBag.maxRow = cinemaHallPlaceData.MaxRow;
+            ViewBag.maxCell = cinemaHallPlaceData.MaxCell;
+            ViewBag.cinemaHallRows = cinemaHallPlaceData.CinemaHallRows;
+
+            return View();
+        }
+    }
+}
---
+++ OnlineCinema/Models/CinemaHallMoviePlace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OnlineCinema.Models
+{
+    public class CinemaHallMoviePlace
+    {
+        // cinema hall movie place ID
+        public int ID { get; set; }
+        // cinema hall movie ID
+        public int CinemaHallMovieID { get; set; }
+        // cinema hall place ID
+        public int CinemaHallPlaceID { get; set; }
+    }
+
+    public class CinemaHallMoviePlaceContext : DbContext
+    {
+        public CinemaHallMoviePlaceContext() : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<CinemaHallMoviePlace> CinemaHallMoviePlaces { get; set; }
+    }
+}
