9678ab3053f01fdf5f52c87eab300d3cf6b05d1d
mkolyn <mkolyn@gmail.com>
2019-04-24 17:51:28 +0300

    book ticket

--- OnlineCinema/Scripts/places.js
+++ OnlineCinema/Scripts/places.js
@@ -4,4 +4,14 @@
     }).mouseleave(function () {
         $('.cinema-hall-cell.joined').removeClass('active');
     });
+
+    $('.cinema-hall-places').on('click', '.cinema-hall-cell', function () {
+        $(this).toggleClass('active');
+
+        if ($('.cinema-hall-cell.active').length > 0) {
+            $('.book-places').removeClass('hidden');
+        } else {
+            $('.book-places').addClass('hidden');
+        }
+    });
 });
