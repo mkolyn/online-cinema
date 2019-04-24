c1786b67a0d81898223bb36627da9c35e820995d
mkolyn <mkolyn@gmail.com>
2019-04-24 17:45:53 +0300

    book ticket

---
+++ OnlineCinema/Scripts/book.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    var cities = $('.cities');
+
+    $('.choose-city').mouseenter(function () {
+        cities.addClass('active');
+    }).mouseleave(function () {
+        cities.removeClass('active');
+    });
+});
---
+++ OnlineCinema/Scripts/places.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('.cinema-hall-cell.joined').mouseenter(function () {
+        $('.cinema-hall-cell.joined[data-group="' + $(this).attr('data-group') + '"]').addClass('active');
+    }).mouseleave(function () {
+        $('.cinema-hall-cell.joined').removeClass('active');
+    });
+});
