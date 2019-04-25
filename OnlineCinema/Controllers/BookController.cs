2d5718aeb34a387c99db4fda903d6885a960e975
mkolyn <mkolyn@gmail.com>
2019-04-25 16:45:26 +0300

    book ticket

--- OnlineCinema/Controllers/BookController.cs
+++ OnlineCinema/Controllers/BookController.cs
@@ -14,6 +14,8 @@ namespace OnlineCinema.Controllers
         private UserContext userDb = new UserContext();
         private CinemaHallMovieContext cinemaHallMovieDb = new CinemaHallMovieContext();
         private CinemaHallPlaceContext cinemaHallPlaceDb = new CinemaHallPlaceContext();
+        private CinemaHallMoviePlaceContext cinemaHallMoviePlaceDb = new CinemaHallMoviePlaceContext();
+        private OrderContext orderDb = new OrderContext();
 
         public ActionResult Index(int id)
         {
@@ -37,13 +39,78 @@ namespace OnlineCinema.Controllers
             ViewBag.movieName = movie.Name;
             ViewBag.date = new DateTime(date.Year, date.Month, date.Day, date.Hour, date.Minute, 0);
 
-            CinemaHallPlaceData cinemaHallPlaceData = cinemaHallPlaceDb.GetCinemaHallPlacesData(cinemaHall.ID);
+            CinemaHallPlaceData cinemaHallPlaceData = cinemaHallPlaceDb.GetCinemaHallPlacesData(cinemaHall.ID, cinemaHallMovie.ID);
 
             ViewBag.maxRow = cinemaHallPlaceData.MaxRow;
             ViewBag.maxCell = cinemaHallPlaceData.MaxCell;
             ViewBag.cinemaHallRows = cinemaHallPlaceData.CinemaHallRows;
+            ViewBag.cinemaHallMovieId = cinemaHallMovie.ID;
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int id, int[] cinemaHallPlaces)
+        {
+            Order order = new Order()
+            {
+                Date = DateTime.Now,
+            };
+            orderDb.Orders.Add(order);
+            orderDb.SaveChanges();
+
+            for (var i = 0; i < cinemaHallPlaces.Length; i++)
+            {
+                OrderItem orderItem = new OrderItem()
+                {
+                    OrderID = order.ID,
+                    CinemaHallMovieID = id,
+                    CinemaHallPlaceID = cinemaHallPlaces[i],
+                };
+                orderDb.OrderItems.Add(orderItem);
+
+                CinemaHallMoviePlace cinemaHallMoviePlace = new CinemaHallMoviePlace()
+                {
+                    CinemaHallMovieID = id,
+                    CinemaHallPlaceID = cinemaHallPlaces[i],
+                    Status = CinemaHallMoviePlace.STATUS_PROCESSING,
+                };
+                cinemaHallMoviePlaceDb.CinemaHallMoviePlaces.Add(cinemaHallMoviePlace);
+            }
+            orderDb.SaveChanges();
+            cinemaHallMoviePlaceDb.SaveChanges();
+
+            return Json(new { id = order.ID });
+        }
+
+        public ActionResult Confirm(int id)
+        {
+            Order order = orderDb.Orders.Find(id);
+            List<OrderItemInfo> orderItems = orderDb.GetOrderItems(id);
+
+            ViewBag.orderItems = orderItems;
+            ViewBag.date = order.Date;
+
+            return View();
+        }
+
+        public ActionResult Success(int id)
+        {
+            Order order = orderDb.Orders.Find(id);
+            order.IsPaid = true;
+            orderDb.SaveChanges();
+
+            List<OrderItemInfo> orderItems = orderDb.GetOrderItems(id);
+            foreach (OrderItemInfo orderItem in orderItems)
+            {
+                CinemaHallMoviePlace cinemaHallMoviePlace = cinemaHallMoviePlaceDb.GetCinemaHallMoviePlace(
+                    orderItem.CinemaHallMovieID, orderItem.CinemaHallPlaceID);
+
+                cinemaHallMoviePlace.Status = CinemaHallMoviePlace.STATUS_SUCCESSFULL;
+                cinemaHallMoviePlaceDb.SaveChanges();
+            }
+
+            return Json("ok", JsonRequestBehavior.AllowGet);
         }
     }
 }
