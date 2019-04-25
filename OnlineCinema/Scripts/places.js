0c3c2fef45693621b55d71684bdc5ffe96562129
mkolyn <mkolyn@gmail.com>
2019-04-25 16:46:06 +0300

    book ticket

--- OnlineCinema/Scripts/places.js
+++ OnlineCinema/Scripts/places.js
@@ -1,17 +1,42 @@
 ﻿$(document).ready(function () {
-    $('.cinema-hall-cell.joined').mouseenter(function () {
-        $('.cinema-hall-cell.joined[data-group="' + $(this).attr('data-group') + '"]').addClass('active');
+    $('.cinema-hall-cell.joined').not('.booked').mouseenter(function () {
+        $('.cinema-hall-cell.joined[data-group="' + $(this).attr('data-group') + '"]').addClass('grouped');
     }).mouseleave(function () {
-        $('.cinema-hall-cell.joined').removeClass('active');
+        $('.cinema-hall-cell.joined').removeClass('grouped');
     });
 
     $('.cinema-hall-places').on('click', '.cinema-hall-cell', function () {
+        if ($(this).hasClass('booked')) {
+            return false;
+        }
+
         $(this).toggleClass('active');
+        var isActive = $(this).hasClass('active');
+        if ($(this).hasClass('joined')) {
+            $('.cinema-hall-cell.joined[data-group="' + $(this).attr('data-group') + '"]').each(function () {
+                isActive ? $(this).addClass('active') : $(this).removeClass('active');
+            });
+        }
 
         if ($('.cinema-hall-cell.active').length > 0) {
             $('.book-places').removeClass('hidden');
         } else {
             $('.book-places').addClass('hidden');
         }
+    });
+
+    $('.book-places').click(function () {
+        $.ajax({
+            url: '/Book/Create/' + $('.cinema-hall-movie-id').val(),
+            method: "POST",
+            data: {
+                cinemaHallPlaces: $.map($('.cinema-hall-cell.active'), function (item) {
+                    return $(item).attr('data-id');
+                }),
+            },
+            success: function (data) {
+                window.location.href = '/Book/Confirm/' + data.id;
+            }
+        });
     });
 });
