cef6b7ed639a55c4a8b5ff332a78545bacf48693
mkolyn <mkolyn@gmail.com>
2019-09-27 20:07:23 +0300

    change core folder

--- OnlineCinema/Scripts/book.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    var cities = $('.cities');
-
-    $('.choose-city').mouseenter(function () {
-        cities.addClass('active');
-    }).mouseleave(function () {
-        cities.removeClass('active');
-    });
-});
--- OnlineCinema/Scripts/home.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    $('.cinema select').change(function () {
-        window.location.href = '/date/' + $('.cinema-year').val() + '/' + $('.cinema-month').val()
-            + '/' + $('.cinema-day').val() + '/' + $(this).val();
-    });
-});
